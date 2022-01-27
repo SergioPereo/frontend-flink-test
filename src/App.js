b95a47914bef21cca562406c35ede8491f240edd
iMAC <sergio.pereo@outlook.com>
2022-01-27 17:39:55 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import axios from "axios";
+import Box from '@mui/material/Box';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
 
 function App() {
+
+  const url = "https://backend-flink-test-9yuk5.ondigitalocean.app/api"
+
+  const [businessData, setBusinessData] = useState([])
+
+  useEffect(() => {
+      axios({
+          url: `${url}/business/companies/`,
+          method: 'get'
+      }).then(response => {
+          const DATA = response.data
+          console.log(DATA)
+          setBusinessData(DATA)
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <div style={{ padding: 30 }}>
+            <Paper style={{padding: 30}}>
+                <Grid
+                    container
+                    spacing={3}
+                >
+                  
+                  <Grid item xs={12}>
+                    <Card sx={{ minWidth: 275 }}>
+                      <CardContent>
+                        <Typography sx={{ fontSize: 26 }} color="text.secondary" gutterBottom>
+                          Cantidad de registros
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                          {businessData.length}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  {businessData.map((business)=>{
+                    return (
+                      <>
+                        <Grid item xs={12}>
+                          <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                              <Typography sx={{ fontSize: 26 }} color="text.secondary" gutterBottom>
+                                {business.id}
+                              </Typography>
+                              <Typography variant="h5" component="div">
+                                {business.name}
+                              </Typography>
+                              <Typography sx={{ fontSize: 26 }} color="text.secondary" gutterBottom>
+                                {business.description}
+                              </Typography>
+                              <Typography variant="h5" component="div">
+                                {business.symbol}
+                              </Typography>
+                              {
+                                business.values.map((value)=> (
+                                  <>
+                                    <Typography sx={{ fontSize: 26 }} color="text.secondary" gutterBottom>
+                                      {`${business.name} value`}
+                                    </Typography>
+                                    <Typography sx={{ fontSize: 26 }} color="text.secondary" gutterBottom>
+                                      {`Id: ${value.id}`}
+                                    </Typography>
+                                    <Typography variant="h5" component="div">
+                                      {value.value}
+                                    </Typography>
+                                  </>
+                                ))
+                              }
+                            </CardContent>
+                          </Card>
+                        </Grid>
+
+
+                      </>
+                    )
+                  })}
+                </Grid>
+            </Paper>
+        </div>
   );
 }
 
